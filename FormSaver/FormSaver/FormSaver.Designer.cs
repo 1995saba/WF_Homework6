c692988034fabd839e69a0eeb5b00aa70c2adf8f
Sabina <sabina.orunbaeva@gmail.com>
2017-10-06 03:18:33 +0600

    Add files via upload

---
+++ FormSaver/FormSaver/FormSaver.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace FormSaver
+{
+    partial class FormSaver
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.labelDateOfBirth = new System.Windows.Forms.Label();
+            this.labelIsMarried = new System.Windows.Forms.Label();
+            this.labelOtherInfo = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dateTimePickerBirthday = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.richTextBoxOtherInfo = new System.Windows.Forms.RichTextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxIsMarried = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSurname.Location = new System.Drawing.Point(99, 34);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(169, 16);
+            this.labelSurname.TabIndex = 0;
+            this.labelSurname.Text = "Введите Вашу фамилию:";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelName.Location = new System.Drawing.Point(137, 75);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(131, 16);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Введите Ваше имя:";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLastName.Location = new System.Drawing.Point(101, 117);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(168, 16);
+            this.labelLastName.TabIndex = 2;
+            this.labelLastName.Text = "Введите Ваше отчество:";
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGender.Location = new System.Drawing.Point(205, 160);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(64, 16);
+            this.labelGender.TabIndex = 3;
+            this.labelGender.Text = "Ваш пол:";
+            // 
+            // labelDateOfBirth
+            // 
+            this.labelDateOfBirth.AutoSize = true;
+            this.labelDateOfBirth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDateOfBirth.Location = new System.Drawing.Point(158, 204);
+            this.labelDateOfBirth.Name = "labelDateOfBirth";
+            this.labelDateOfBirth.Size = new System.Drawing.Size(110, 16);
+            this.labelDateOfBirth.TabIndex = 4;
+            this.labelDateOfBirth.Text = "Дата рождения:";
+            // 
+            // labelIsMarried
+            // 
+            this.labelIsMarried.AutoSize = true;
+            this.labelIsMarried.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelIsMarried.Location = new System.Drawing.Point(143, 250);
+            this.labelIsMarried.Name = "labelIsMarried";
+            this.labelIsMarried.Size = new System.Drawing.Size(125, 16);
+            this.labelIsMarried.TabIndex = 5;
+            this.labelIsMarried.Text = "Семейный статус:";
+            // 
+            // labelOtherInfo
+            // 
+            this.labelOtherInfo.AutoSize = true;
+            this.labelOtherInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelOtherInfo.Location = new System.Drawing.Point(61, 298);
+            this.labelOtherInfo.Name = "labelOtherInfo";
+            this.labelOtherInfo.Size = new System.Drawing.Size(207, 16);
+            this.labelOtherInfo.TabIndex = 6;
+            this.labelOtherInfo.Text = "Дополнительная информация:";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Location = new System.Drawing.Point(304, 391);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(119, 26);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // dateTimePickerBirthday
+            // 
+            this.dateTimePickerBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePickerBirthday.Location = new System.Drawing.Point(304, 204);
+            this.dateTimePickerBirthday.Name = "dateTimePickerBirthday";
+            this.dateTimePickerBirthday.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerBirthday.TabIndex = 8;
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBoxGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Items.AddRange(new object[] {
+            "Мужской",
+            "Женский"});
+            this.comboBoxGender.Location = new System.Drawing.Point(304, 160);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxGender.TabIndex = 9;
+            // 
+            // richTextBoxOtherInfo
+            // 
+            this.richTextBoxOtherInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxOtherInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxOtherInfo.Location = new System.Drawing.Point(304, 298);
+            this.richTextBoxOtherInfo.Name = "richTextBoxOtherInfo";
+            this.richTextBoxOtherInfo.Size = new System.Drawing.Size(345, 62);
+            this.richTextBoxOtherInfo.TabIndex = 11;
+            this.richTextBoxOtherInfo.Text = "";
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSurname.Location = new System.Drawing.Point(304, 34);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(224, 22);
+            this.textBoxSurname.TabIndex = 12;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(304, 75);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(224, 22);
+            this.textBoxName.TabIndex = 13;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLastName.Location = new System.Drawing.Point(304, 117);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(224, 22);
+            this.textBoxLastName.TabIndex = 14;
+            // 
+            // textBoxIsMarried
+            // 
+            this.textBoxIsMarried.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxIsMarried.Location = new System.Drawing.Point(304, 250);
+            this.textBoxIsMarried.Name = "textBoxIsMarried";
+            this.textBoxIsMarried.Size = new System.Drawing.Size(224, 22);
+            this.textBoxIsMarried.TabIndex = 15;
+            // 
+            // FormSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.textBoxIsMarried);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.richTextBoxOtherInfo);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.dateTimePickerBirthday);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelOtherInfo);
+            this.Controls.Add(this.labelIsMarried);
+            this.Controls.Add(this.labelDateOfBirth);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelSurname);
+            this.Name = "FormSaver";
+            this.Text = "Анкета";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.Label labelDateOfBirth;
+        private System.Windows.Forms.Label labelIsMarried;
+        private System.Windows.Forms.Label labelOtherInfo;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthday;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.RichTextBox richTextBoxOtherInfo;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxIsMarried;
+    }
+}
+
---
+++ FormSaver/FormSaver/FormSaver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FormSaver
+{
+    public partial class FormSaver : Form
+    {
+        public FormSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            using(FileStream fstream=new FileStream(@"form.txt", FileMode.OpenOrCreate))
+            {
+                fstream.Seek(0, SeekOrigin.End);
+
+                byte[] information = Encoding.Default.GetBytes(textBoxSurname.Text + " "
+                    + textBoxName.Text + " " + textBoxLastName.Text
+                    + " Пол: " + comboBoxGender.Text
+                    + " Дата рождения: " + dateTimePickerBirthday.Text
+                    + " Статус: " + textBoxIsMarried.Text
+                    + " Дополнительная информация: " + richTextBoxOtherInfo.Text);
+
+                fstream.Write(information, 0, information.Length);
+
+                textBoxSurname.Text = "";
+                textBoxName.Text = "";
+                textBoxLastName.Text = "";
+                comboBoxGender.Text = "";
+                dateTimePickerBirthday.Text = "";
+                textBoxIsMarried.Text = "";
+                richTextBoxOtherInfo.Text = "";
+            }
+        }
+    }
+}
